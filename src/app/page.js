f9b4e050f9a5c8dc8cf8a273eb6a496350150618
Naeem <inaeem@protonmail.com>
2024-06-23 19:43:32 +0500

    setup layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,3 @@
-import PostExcerpt from '@/components/PostExcerpt';
-import { Grid } from '@mui/material';
-
-const getPosts = async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-  return await response.json();
-};
-
-export default async function Home() {
-  let posts = await getPosts();
-  return (
-    <Grid container spacing={1}>
-      {posts.map(post => (
-        <Grid item sm={12} md={6} lg={4} xl={4}>
-          <PostExcerpt key={post.id} post={post} />
-        </Grid>
-      ))}
-    </Grid>
-  );
+export default function Home() {
+  return <h1>Home</h1>;
 }
