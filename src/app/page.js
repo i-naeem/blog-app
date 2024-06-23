93c3dc105775fd0eefebc4c9e570d4e8fb5888c1
Muhammad Naeem <inaeem@protonmail.com>
2024-06-23 21:28:39 +0500

    added api

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <h1>Home</h1>;
+export default async function Home() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await response.json();
+
+  return (
+    <section>
+      {posts.map((post) => (
+        <h1 key={post.id}>{post.title}</h1>
+      ))}
+    </section>
+  );
 }
